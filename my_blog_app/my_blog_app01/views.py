bd451b4c6e6429d487b170c24bee5e54e9e719ba
SetterUk <Koushik4067@gmail.com>
2025-06-28 05:56:56 +0530

    Add complete delete and edit functionality for blog posts
    - Added forms.py for handling post creation and editing
    - Added update_post.html template for editing posts
    - Updated views.py with DeletePostView and UpdatePostView
    - Updated URLs configuration for delete and edit routes
    - Enhanced article_details.html with edit and delete buttons

--- my_blog_app/my_blog_app01/views.py
+++ my_blog_app/my_blog_app01/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
 from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def home(request):
@@ -9,6 +11,7 @@ from .models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-date_posted']  # Order by ID in descending order
 
 
 class articleDetailView(DetailView):
@@ -18,5 +21,18 @@ class articleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
+    # fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    fields = ['title', 'title_tag', 'body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    fields = ['title', 'title_tag', 'body']
+    success_url = reverse_lazy('home')
